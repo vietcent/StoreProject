7fd2998c5db9a8701e7e2497df2f738d0c351640
Vincent Nguyen <vqnguyen16@gmail.com>
2015-05-26 15:17:09 -0700

    Added Item and Customer class

--- src/StoreMain.java
+++ src/StoreMain.java
@@ -3,7 +3,20 @@ public class StoreMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Login();
+		
 
+	}
+	
+	private static void Login()
+	{
+		System.out.println("1. Login\n 2. Exit");
+	}
+	
+	private static void MainMenu()
+	{
+		System.out.println("");
 	}
 
 }
